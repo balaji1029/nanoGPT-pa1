1f87ad64ba43ce23ed95fa599ca01b0d5bdeb99c
Balaji <balajikaredla4@gmail.com>
2026-01-12 11:19:31 +0530

    add input tokens

--- sample.py
+++ sample.py
@@ -82,26 +82,34 @@ x = (torch.tensor(start_ids, dtype=torch.long, device=device)[None, ...])
 tokens_len = []
 time_taken = []
 # run generation
+different_input_tokens = [
+    "Hello, how are you?",
+    "What is the capital of France?",
+    "Once upon a time in a land far away..."
+    "In a shocking discovery, scientists have found that"
+    "The quick brown fox jumps over the lazy dog."
+]
 with torch.no_grad():
     with ctx:
-        for k in range(num_samples):
-            mem_before = torch.cuda.memory_allocated() if device_type == 'cuda' else 0
-            y, time_list = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k)
-            mem_after = torch.cuda.memory_allocated() if device_type == 'cuda' else 0
-            # print(decode(y[0].tolist()))
-            # print('---------------')
-            num_tokens = list(range(len(x[0]), len(y[0] - len(x[0]))))
-            tokens_len += num_tokens[1:]
-            time_taken += time_list[1:]
-            print(len(y[0]), 'tokens generated while max_new_tokens is', max_new_tokens)
-            print(f"Generated token IDs: {num_tokens}")
-            print(f"Generation times (in seconds): {time_list}")
-            print(f"Memory usage (in bytes): {mem_after - mem_before}")
+        for prompt in different_input_tokens:
+            for k in range(num_samples):
+                mem_before = torch.cuda.memory_allocated() if device_type == 'cuda' else 0
+                y, time_list = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k)
+                mem_after = torch.cuda.memory_allocated() if device_type == 'cuda' else 0
+                # print(decode(y[0].tolist()))
+                # print('---------------')
+                num_tokens = list(range(len(x[0]), len(y[0] - len(x[0]))))
+                tokens_len += num_tokens[1:]
+                time_taken += time_list[1:]
+                print(len(y[0]), 'tokens generated while max_new_tokens is', max_new_tokens)
+                print(f"Generated token IDs: {num_tokens}")
+                print(f"Generation times (in seconds): {time_list}")
+                print(f"Memory usage (in bytes): {mem_after - mem_before}")
 
 import matplotlib.pyplot as plt
 plt.figure(figsize=(10, 5))
 # truncate it to y_max for better visualization
-y_max = 0.020
+# y_max = 0.020
 plt.scatter(tokens_len, time_taken, alpha=0.5)
 plt.title('Time taken vs Number of tokens given as input')
 plt.xlabel('Number of tokens given as input')
