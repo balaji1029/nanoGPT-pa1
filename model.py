1d5113c16ee0a3a3b0c1438af067bb184c4a6ce6
Balaji <balajikaredla4@gmail.com>
2026-02-09 01:03:38 +0530

    idkk

--- model.py
+++ model.py
@@ -82,8 +82,8 @@ class CausalSelfAttention(nn.Module):
             # if not cached_kv:
             if cached_kv:
                 print(att.shape)
-                bias = self.tril(torch.ones(1, 1, Tk, Tk)).to(att.device)
-                att = att.masked_fill(bias[:, :, -T:, -T] == 0, float('-inf'))
+                bias = torch.tril(torch.ones(1, 1, Tk, Tk)).to(att.device)
+                att = att.masked_fill(bias[:, :, -T:, -Tk] == 0, float('-inf'))
             else:
                 att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
             att = F.softmax(att, dim=-1)
