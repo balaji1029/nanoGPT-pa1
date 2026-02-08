c85cb285566ca65e3ba5fac58b31d5ed48f8ef80
Balaji <balajikaredla4@gmail.com>
2026-02-08 22:50:22 +0530

    remove flash

--- model.py
+++ model.py
@@ -72,22 +72,22 @@ class CausalSelfAttention(nn.Module):
         v = v.view(B, Tk, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
 
         # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
-        if self.flash and not cached_kv:
-            # efficient attention using Flash Attention CUDA kernels
-            y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=True)
+        # if self.flash and (not cached_kv):
+        #     # efficient attention using Flash Attention CUDA kernels
+        #     y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=True)
+        # else:
+        print('Reached here because caching')
+        # manual implementation of attention
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        # if not cached_kv:
+        if cached_kv:
+            mask = torch.tril(torch.ones(T, Tk, device=q.device))
+            att = att.masked_fill(mask == 0, float('-inf'))
         else:
-            print('Reached here because caching')
-            # manual implementation of attention
-            att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-            # if not cached_kv:
-            if cached_kv:
-                mask = torch.tril(torch.ones(T, Tk, device=q.device))
-                att = att.masked_fill(mask == 0, float('-inf'))
-            else:
-                att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
-            att = F.softmax(att, dim=-1)
-            att = self.attn_dropout(att)
-            y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+            att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+        att = F.softmax(att, dim=-1)
+        att = self.attn_dropout(att)
+        y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
         y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
 
         # output projection
